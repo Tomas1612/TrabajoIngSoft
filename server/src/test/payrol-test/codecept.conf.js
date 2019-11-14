a20a4d5ebf4d7ed377a9501cdebac606d6e17b4d
Tomas1612 <1600351@ucc.edu.ar>
2019-11-14 15:39:07 -0300

    Integracion

---
+++ server/src/test/payrol-test/codecept.conf.js
@@ -0,0 +1,31 @@
+exports.config = {
+	tests: "./*_test.js",
+	output: "./output",
+	helpers: {
+		REST: {
+			endpoint: "https://aqueous-river-20462.herokuapp.com/",
+			onRequest: () => {
+			}
+		}
+	},
+	include: {},
+	bootstrap: null,
+	mocha: {
+	"reporterOptions": {
+      	"codeceptjs-cli-reporter": {
+        "stdout": "-",
+        "options": {
+          "steps": true,
+        }
+      },
+      "mocha-junit-reporter": {
+        "stdout": "./output/console.log",
+        "options": {
+          "mochaFile": "./output/result.xml"
+        },
+        "attachments": true //add screenshot for a failed test
+		  }
+		}
+	},
+	name: "codeceptjs-rest"
+};
